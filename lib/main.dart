c86684e0f74cef1786c900a2ea6278ad55cdd490
Abhishek sahu <Abhishek4032sahu@gmail.com>
2021-12-19 19:27:33 +0530

    day3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:first_app/home_page.dart';
+import 'package:first_app/pages/home_page.dart';
+import 'package:first_app/pages/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,10 +14,22 @@ class MyApp extends StatelessWidget {
   // var day = "tuesday";
   // static const pis = 3.14;
 
-  @override
+  @override //override means you have to rewrriten or overrite the build method;
+  //buildcontext context is a perameter ehich shows location of every component in a myapp tree;
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      // home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/Login": (context) => LoginPage(),
+      }, //routes bassicaly is a path in which we write the direction of pages like which page should be open after which page.
     );
   }
 }
